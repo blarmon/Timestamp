9b0ba46c035ed38e7d2803f4482d7e3409bd144c
Chris Siegel <c.siegel1991@gmail.com>
2016-10-11 22:07:58 +0000

    created and served page to be loaded when no parameters are given

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express')
+var moment = require('moment')
+var fs = require('fs');
+var path = require('path');
 var app = express();
 
+
+
+var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    var fileName = path.join(__dirname, 'noHeader.html')
+    res.sendfile(fileName, function(err) {
+       if (err) {
+           console.log(err);
+       }
+       else {
+           console.log("file sent")
+       }
+   });
+   
 });
 
-app.listen(8080, function () {
+app.get('/:date', function(req, res){
+    res.end(req.params.date);
+    
+})
+
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 8080!');
 });
